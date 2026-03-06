3a10af96516bcebe0004bd781399c1d63bb9402e
cesargrobles <cesargrobles@student.ie.edu>
2026-03-06 10:36:33 +0100

    Validate.py implemented by Cesar

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Module: Main Pipeline
 ---------------------
 Role: Orchestrate the entire flow (Load -> Clean -> Validate -> Train -> Evaluate).
-Usage: python -m src.main
+Usage: python src/main.py
 """
 if __name__ == "__main__":
     print("Pipeline not implemented yet.")
